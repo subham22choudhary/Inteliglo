59ec7608ff4e7e3b9ce5bddcf362186a1192b2ad
subham22choudhary <58947617+subham22choudhary@users.noreply.github.com>
2026-04-23 01:15:12 +0530

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Orbitron, Syne, JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 
@@ -50,6 +51,50 @@ export default function RootLayout({
       lang="en"
       className={`${orbitron.variable} ${syne.variable} ${jetbrainsMono.variable}`}
     >
+
+      <head>
+
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s){
+        if(f.fbq)return;
+        n=f.fbq=function(){
+          n.callMethod
+            ? n.callMethod.apply(n,arguments)
+            : n.queue.push(arguments)
+        };
+        if(!f._fbq)f._fbq=n;
+        n.push=n;
+        n.loaded=!0;
+        n.version='2.0';
+        n.queue=[];
+        t=b.createElement(e);
+        t.async=!0;
+        t.src=v;
+        s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)
+      }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '4330339817239647');
+      fbq('track', 'PageView');
+    `
+          }}
+        />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=4330339817239647&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+
+      </head>
       <body style={{ fontFamily: 'var(--font-syne), sans-serif' }}>
         {children}
       </body>
